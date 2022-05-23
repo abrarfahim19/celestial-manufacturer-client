8521eed07c7d8f1357476d4e18bba5f03e1d72ee
abrarfahim19 <fahimpatwari19@gmail.com>
2022-05-23 12:18:50 +0600

    carousel

---
+++ src/Components/Pages/Home/Carousel.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Carousel1 from '../../../Assets/carousel1.png';
+import Carousel2 from '../../../Assets/carousel2.png';
+import Carousel3 from '../../../Assets/carousel3.png';
+
+const Carousel = () => {
+  return (
+    <div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+  <img src={Carousel1} alt='bike' class="w-full"/> 
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+  <img src={Carousel2} alt='bike' class="w-full"/> 
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src={Carousel3} alt='bike' class="w-full"/> 
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+  );
+};
+
+export default Carousel;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import Carousel from './Carousel'
 const Home = () => {
     return (
         <div>
+            <Carousel></Carousel>
             <h2>This is Home</h2>
             <i class="fa-solid fa-microchip text-5xl text-primary"></i>
         </div>
