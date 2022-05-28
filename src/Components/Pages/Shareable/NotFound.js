ad956cceac89ba8a74c9dfc768378b6630098953
abrarfahim19 <fahimpatwari19@gmail.com>
2022-05-28 23:59:03 +0600

    not found

--- src/Components/Pages/Shareable/NotFound.js
+++ src/Components/Pages/Shareable/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404| Not Found</h2>
+        <div className='row d-flex align-items-center'>
+            <h1 className='col-md-6 text-center mt-5 mb-5'>Page is Missing!!!<br /> 🤕</h1>
+            <img className='col-md-6 w-25' src={"https://i.ibb.co/WVYLqF0/notfound.jpg"} alt="Not Found" />
         </div>
     );
 };
